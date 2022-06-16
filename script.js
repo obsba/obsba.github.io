20c07a4395f56a61131ddcfcfaf7a1ba2389a5c5
obsba <107607395+obsba@users.noreply.github.com>
2022-06-16 08:41:58 +0200

    scriptj.

---
+++ script.js
@@ -0,0 +1,23 @@
+class AddElement {
+    constructor(type) {
+        this.type = type
+    }
+
+    to(dest) {
+        this.to = to
+        return this
+    }
+
+    content(content) {
+        this.content = content
+        return this
+    }
+
+    done() {
+        const parent = document.querySelector(this.to)
+        const element = new HTMLElement(this.type)
+        element.innerHTML = this.content
+    }
+}
+
+const add = new AddElement("h1").to("h1").content("LOL").done()
